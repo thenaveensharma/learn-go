16d9be9fa9f3005ee89addc831ecb97a86b11030
thenaveensharma <naveenchaurasiya1501@gmail.com>
2025-04-06 00:00:32 +0530

    learn. methods on struct

--- hello-world.go
+++ hello-world.go
@@ -273,6 +273,26 @@ func main() {
 		true,
 	}
 	fmt.Println(dog)
+	//Methods
+
+	r := rect{width: 10, height: 20}
+	fmt.Println("area: ", r.area())
+	fmt.Println("perim: ", r.perim())
+	rp := &r
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim: ", rp.perim())
+
+}
+
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int {
+	return r.height * r.width
+}
+func (r rect) perim() int {
+	return 2 * (r.width + r.height)
 }
 
 type person struct {
