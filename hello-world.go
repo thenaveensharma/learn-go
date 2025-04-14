741aabb1d5d8c9a129e0e9586a2abfcdbafe49cd
thenaveensharma <naveenchaurasiya1501@gmail.com>
2025-04-14 23:56:00 +0530

    learn. interface

--- hello-world.go
+++ hello-world.go
@@ -282,6 +282,53 @@ func main() {
 	fmt.Println("area: ", rp.area())
 	fmt.Println("perim: ", rp.perim())
 
+	//Interface
+	rectangle := rectangle{width: 2.1, height: 3.1111}
+	circle := circle{radius: 3.3}
+
+	detectCircle(circle)
+	detectCircle(rectangle)
+	measure(circle)
+	measure(rectangle)
+}
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+type circle struct {
+	radius float64
+}
+
+func (r rectangle) area() float64 {
+	return r.height * r.width
+
+}
+func (r rectangle) perim() float64 {
+	return 2 * (r.height + r.width)
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func detectCircle(g geometry) {
+	if c, ok := g.(circle); ok {
+		fmt.Println("circle with radius r: ", c.radius)
+	}
 }
 
 type rect struct {
