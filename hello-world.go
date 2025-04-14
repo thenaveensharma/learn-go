3539fbf07f83a246da70be5f785418a7f8e23c00
thenaveensharma <naveenchaurasiya1501@gmail.com>
2025-04-15 00:43:06 +0530

    learn. Enums

--- hello-world.go
+++ hello-world.go
@@ -290,6 +290,46 @@ func main() {
 	detectCircle(rectangle)
 	measure(circle)
 	measure(rectangle)
+
+	//Enums
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+	ns2 := transition(ns)
+	fmt.Println(ns2)
+
+}
+
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
 }
 
 type geometry interface {
